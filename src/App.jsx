cad2ecca28e84389155b6d56cdab2f735a556105
David Fernández <davidfernandezcomesana@gmail.com>
2023-01-24 01:04:33 +0100

    update Firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './style';
-
+//All imports
 import {
   Navbar, 
   Hero, 
---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDRmsmV9nOHNEHjy2002A3ebqHwvTQoyXM",
+    authDomain: "modern-landing-page.firebaseapp.com",
+    projectId: "modern-landing-page",
+    storageBucket: "modern-landing-page.appspot.com",
+    messagingSenderId: "33355703800",
+    appId: "1:33355703800:web:7764a2d61e53f2df36a20d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
